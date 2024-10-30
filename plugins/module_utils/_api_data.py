hirnpfirsich/community.routeros

commit 710bf1871bb9f8619a6f3c0908ebe8231761ac97

Add INCOMPLETE support for "container"
* primary key handling does not work correct
* detection of changes in `remote-image` of `file` do not work

plugins/module_utils/_api_data.py
@@ -5163,6 +5163,38 @@ PATHS = {
             },
         ),
     ),
+    ('container', ): APIData(
+        versioned=[
+            # available since 7.4beta4
+            ('7.4', '>', VersionedAPIData(
+                primary_keys=('name',),
+                fully_understood=True,
+                fields={
+                    'cmd': KeyInfo(),
+                    'comment': KeyInfo(can_disable=True, remove_value=''),
+                    'dns': KeyInfo(default=''),
+                    'domain-name': KeyInfo(),
+                    'entrypoint': KeyInfo(),
+                    'envlist': KeyInfo(),
+                    'file': KeyInfo(write_only=True),
+                    'hostname': KeyInfo(),
+                    'interface': KeyInfo(required=True),
+                    'logging': KeyInfo(),
+                    'mounts': KeyInfo(default=''),
+                    'remote-image': KeyInfo(write_only=True),
+                    'root-dir': KeyInfo(),
+                    'start-on-boot': KeyInfo(),
+                    'stop-signal': KeyInfo(),
+                    'user': KeyInfo(),
+                    'workdir': KeyInfo(),
+                    'name': KeyInfo(read_only=True),
+                    'tag': KeyInfo(read_only=True, automatically_computed_from='remote-image'),
+                    'os': KeyInfo(read_only=True),
+                    'arch': KeyInfo(read_only=True),
+                },
+            )),
+        ],
+    ),
     ('container', 'config'): APIData(
         versioned=[
             # available since 7.4beta4

commit 4e04d0a532663d2472f14b83736581f46bcf598d

container: no primary key
Fixes: 710bf1871bb9f8619a6f3c0908ebe8231761ac97

plugins/module_utils/_api_data.py
@@ -5231,7 +5231,6 @@ PATHS = {
         versioned=[
             # available since 7.4beta4
             ('7.4', '>', VersionedAPIData(
-                primary_keys=('name',),
                 fully_understood=True,
                 fields={
                     'cmd': KeyInfo(),