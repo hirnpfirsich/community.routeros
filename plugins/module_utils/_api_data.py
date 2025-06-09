hirnpfirsich/community.routeros

commit b5c8a7ecd8e4728b25e7b2ae72283eff1360e607

interface ethernet: add sfp-ignore-tx-los

plugins/module_utils/_api_data.py
@@ -374,6 +374,7 @@ PATHS = {
                 'power-cycle-ping-timeout': KeyInfo(can_disable=True),
                 'rx-flow-control': KeyInfo(default='off'),
                 'sfp-rate-select': KeyInfo(default='high'),
+                'sfp-ignore-rx-los': KeyInfo(default='no'),
                 'sfp-shutdown-temperature': KeyInfo(default=95),
                 'speed': KeyInfo(),
                 'tx-flow-control': KeyInfo(default='off'),

commit eeb57936686d326534f6f8e877aed53c15b01911

interface ethernet: fix sfp-ignore-rx-los
Fixes: b5c8a7ecd8e4728b25e7b2ae72283eff1360e607

plugins/module_utils/_api_data.py
@@ -374,7 +374,7 @@ PATHS = {
                 'power-cycle-ping-timeout': KeyInfo(can_disable=True),
                 'rx-flow-control': KeyInfo(default='off'),
                 'sfp-rate-select': KeyInfo(default='high'),
-                'sfp-ignore-rx-los': KeyInfo(default='no'),
+                'sfp-ignore-rx-los': KeyInfo(default=False),
                 'sfp-shutdown-temperature': KeyInfo(default=95),
                 'speed': KeyInfo(),
                 'tx-flow-control': KeyInfo(default='off'),